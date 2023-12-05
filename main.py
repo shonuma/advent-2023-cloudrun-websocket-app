4fc7f7583b60159379d48cbcf479071b18e28b44
Sho Onuma <onumasho@google.com>
2023-12-05 14:33:56 +0900

    init

---
+++ main.py
@@ -0,0 +1,264 @@
+import asyncio
+import base64
+import json
+import time
+
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from models.constants import gcp_icons
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+users = {}
+answers = {}
+begin_time = 0.0
+
+# https://fastapi.tiangolo.com/advanced/websockets/
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+# static folder
+# https://fastapi.tiangolo.com/tutorial/static-files/
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def lobby(request: Request):
+    return templates.TemplateResponse(
+        "lobby.html",
+        {
+            "request": request,
+            "hostname": ""
+        }
+    )
+
+# ゲームのステータスを管理する global 変数
+status = {
+    "isKarutaStarted": False,
+    "isHinting": False,
+    "isHintEnded": False
+}
+
+
+@app.get("/game/{base64d_user_id}", response_class=HTMLResponse)
+async def game(request: Request, base64d_user_id: str):
+    user_id = base64.b64decode(base64d_user_id).decode()
+    return templates.TemplateResponse(
+        "game.html",
+        {
+            "request": request,
+            "base64d_user_id": base64d_user_id,
+            "user_id": user_id,
+            "icons": gcp_icons,
+        }
+    )
+
+
+def is_parent(client_id: str):
+    return users[client_id] == 10
+
+
+def get_description(name: str):
+    for icon in gcp_icons:
+        if icon["name"] == name:
+            return icon["desc"]
+
+
+def get_current_status():
+    for client_id in users.keys():
+        if users[client_id] == 10:
+            status["isKarutaStarted"] = True
+            break
+
+    return status
+
+
+def create_message(message_type: str, value, user_name="system"):
+    return json.dumps(
+        {
+            "messageType": message_type,
+            "user": user_name,
+            "value": value
+        }
+    )
+
+
+def game_init():
+    global status
+    status = {
+        "isKarutaStarted": False,
+        "isHinting": False,
+        "isHintEnded": False
+    }
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket)
+    user_name = base64.b64decode(client_id).decode()
+    users[client_id] = 1
+    await manager.send_personal_message(
+        create_message(
+            "UI",
+            get_current_status(),
+        ),
+        websocket,
+    )
+    try:
+        while True:
+            data = await websocket.receive_text()
+            global answers
+            global status
+            global begin_time
+            # かるたゲームを始める
+            user_name = base64.b64decode(client_id).decode()
+            # protocol の解析
+            if data.startswith("[Choose]"):
+                chosen = data.split(',')[1].split('-')[1]
+                if status["isHinting"] and not status["isHintEnded"]:
+                    if not is_parent(client_id):
+                        if user_name not in answers:
+                            # 0秒未満は0秒に丸める
+                            answered_time = round(time.time() - begin_time, 3)
+                            if answered_time < 0:
+                                answered_time = 0
+                            answers[user_name] = {
+                                "chosen": chosen,
+                                "answeredTime": answered_time
+                            }
+                            await manager.broadcast(
+                                create_message(
+                                    "SYSTEM_GAME",
+                                    f"{user_name} が {chosen} を選択！"
+                                )
+                            )
+                        else:
+                            # 選択済みなのでスルー
+                            pass
+                else:
+                    status["isHinting"] = True
+                    answers = {}
+                    if is_parent(client_id):
+                        # is_hinting
+                        chosen = data.split(',')[1].split('-')[1]
+                        desc = get_description(chosen)
+                        await manager.send_personal_message(
+                            create_message(
+                                "SYSTEM",
+                                f"{chosen} を選択。問題を送信します。"
+                            ),
+                            websocket
+                        )
+                        begin_time = time.time()
+                        await manager.broadcast(
+                            create_message("HINT_START", {})
+                        )
+                        await asyncio.sleep(1)
+                        for word in desc.split(" "):
+                            await manager.broadcast(
+                                create_message(
+                                    "HINT",
+                                    word,
+                                )
+                            )
+                            await asyncio.sleep(1)
+                        status["isHintEnded"] = True
+                        await manager.broadcast(
+                            create_message("HINT_END", {})
+                        )
+                        await asyncio.sleep(1)
+                        # 正解を broadcast
+                        await manager.broadcast(
+                            create_message(
+                                "SYSTEM_GAME",
+                                f"正解は: [{chosen}] でした。"
+                            )
+                        )
+                        # 回答している人がいたら正答判定を送る
+                        if answers:
+                            for user_name in answers.keys():
+                                if answers[user_name]["chosen"] == chosen:
+                                    uat = answers[user_name]['answeredTime']
+                                    await manager.broadcast(
+                                        create_message(
+                                            "SYSTEM_GAME",
+                                            f"{user_name} さん、正解です！ タイム[{uat}] sec"
+                                        )
+                                    )
+                        await asyncio.sleep(1)
+                        await manager.broadcast(
+                            create_message(
+                                "SYSTEM",
+                                "次のゲーム開始までお待ち下さい。"
+                            )
+                        )
+                        game_init()
+            elif data == "[StartGame]":
+                # 親を決める
+                users[client_id] = 10
+                await manager.send_personal_message(
+                    create_message(
+                        "SYSTEM",
+                        "かるたゲームをはじめます。あなたがゲームマスターです。Google Cloud のいずれかのアイコンを選択してください。",
+                    ),
+                    websocket
+                )
+                await manager.broadcast(
+                    create_message(
+                        "START_GAME",
+                        "",
+                    )
+                )
+                await manager.broadcast(
+                    create_message(
+                        "SYSTEM",
+                        f"{user_name} さんがかるたゲームを始めました！開始までもう少々お待ち下さい",
+                    )
+                )
+            else:
+                await manager.send_personal_message(
+                    create_message(
+                        "SYSTEM",
+                        f"You wrote: {data}",
+                    ),
+                    websocket
+                )
+                await manager.broadcast(
+                    create_message(
+                        "MESSAGE",
+                        data,
+                        user_name=user_name
+                    )
+                )
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        users.pop(client_id, None)
+        await manager.broadcast(
+            create_message(
+                "SYSTEM",
+                f"Client #{client_id} left the chat."
+            )
+        )
---
+++ models/constants.py
@@ -0,0 +1,20 @@
+
+
+gcp_icons = (
+    {"name": "cloud_run", "desc": "Fully managed platform for containerized applications"},
+    {"name": "cloud_logging", "desc": "Fully managed, real-time log management with storage, search, analysis and alerting at exabyte scale."},
+    {"name": "bigquery", "desc": "Cloud data warehouse to power your data-driven innovation"},
+    {"name": "cloud_sql", "desc": "Focus on your application, and leave the database to us."},
+    {"name": "cloud_spanner", "desc": "Future-proof your applications with a no-compromise database"},
+    {"name": "cloud_storage", "desc": "Object storage for companies of all sizes"},
+    {"name": "cloud_tasks", "desc": "Asynchronous task execution."},
+    {"name": "firestore", "desc": "Easily develop rich applications using a fully managed, scalable, and serverless document database."},
+    {"name": "compute_engine", "desc": "Secure and customizable compute service that lets you create and run virtual machines on Google’s infrastructure."},
+    {"name": "dataflow", "desc": "Unified stream and batch data processing that's serverless, fast, and cost-effective."},
+    {"name": "pubsub", "desc": "Ingest events for streaming into BigQuery, data lakes or operational databases."},
+    {"name": "vertexai", "desc": "Innovate faster with enterprise-ready generative AI"},
+    {"name": "looker", "desc": "Your unified business intelligence platform. Self-service. Governed. Embedded."},
+    {"name": "cloud_functions", "desc": "Run your code in the cloud with no servers or containers to manage with our scalable, pay-as-you-go functions as a service (FaaS) product."},
+    {"name": "cloud_build", "desc": "Build, test, and deploy on our serverless CI/CD platform."},
+    {"name": "bigtable", "desc": "HBase-compatible, enterprise-grade NoSQL database service with single-digit millisecond latency, limitless scale, and 99.999% availability for large analytical and operational workloads."},
+)
